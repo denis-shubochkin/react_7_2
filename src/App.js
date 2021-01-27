13e859f764a442c4e15585542379dfda6b44e95d
Denis <zippo1095@gmail.com>
2021-01-27 16:22:26 +0300

    react_7_2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,89 @@
 import React, { useState } from 'react';
-import List from './components/List/List';
+
+function Video(props) {
+    return (
+        <div className="item item-video">
+            <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <p className="views">Просмотров: {props.views}</p>
+        </div>
+    )
+};
 
  
+function Article(props) {
+    return (
+        <div className="item item-article">
+            <h3><a href="#">{props.title}</a></h3>
+            <p className="views">Прочтений: {props.views}</p>
+        </div>
+    )
+};
+
+function New(props) {
+      return (
+        <div className="wrap-item wrap-item-new">
+            <span className="label">New!</span>
+            {props.children}
+        </div>
+    )
+}
+
+function Popular(props) {
+      return (
+        <div className="wrap-item wrap-item-popular">
+              <span className="label">Popular!</span>
+              {props.children}
+          </div>
+      )
+  };
+
+  function Detector(Component) {
+      const Upgraded = (props) => {
+        if (props.views<100)
+        {
+            return (
+            <New>
+              <Component {...props} />
+            </New>
+            )
+            
+        }
+        else if(props.views>1000)
+        {
+            return (
+            <Popular>
+                <Component {...props} />
+            </Popular>
+            )
+        }
+        else
+        {
+            return <Component {...props} />
+        }
+    }
+    return Upgraded;
+  }
+
+  let VideoDetector = Detector(Video);
+  let ArticleDetector = Detector(Article);
+
+function List(props) {
 
 
+    return props.list.map(item => {
+        switch (item.type) {
+            case 'video':
+                return (
+                    <VideoDetector {...item} />
+                );
 
-
+            case 'article':
+                return (
+                    <ArticleDetector {...item} />
+                );
+        }
+    });
+};
 
 
 export default function App() {
